ba7576b293d82845fe475540cd93bdc1b5047b07
seowonn <sonseowon5@gmail.com>
2024-11-22 16:44:27 +0900

    Feat: practice04 문제 풀이 작성 (6번 제외)

---
+++ practice04/src/main/java/prob01/Printer.java
@@ -0,0 +1,21 @@
+package prob01;
+
+public class Printer {
+	
+	public void println(int num) {
+		System.out.println(num);
+	}
+	
+	public void println(boolean flag) {
+		System.out.println(flag);
+	}
+	
+	public void println(double doubleNum) {
+		System.out.println(doubleNum);
+	}
+	
+	public void println(String str) {
+		System.out.println(str);
+	}
+
+}
