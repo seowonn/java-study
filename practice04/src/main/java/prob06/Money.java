0bf954f0fdaf04ab5eb77f36efc811381a91dd79
seowonn <sonseowon5@gmail.com>
2024-12-05 16:59:46 +0900

    Fix: 오류 수정

--- practice04/src/main/java/prob06/Money.java
+++ practice04/src/main/java/prob06/Money.java
@@ -1,7 +1,26 @@
 package prob06;
 
 public class Money {
+
 	private int amount;
 	
-	/* 코드 작성 */
+	public Money(int amount) {
+		this.amount = amount;
+	}
+
+	public Object add(Money money) {
+		return new Money(amount + money.amount);
+	}
+
+	public Object minus(Money money) {
+		return new Money(amount - money.amount);
+	}
+
+	public Object multiply(Money money) {
+		return new Money(amount * money.amount);
+	}
+
+	public Object divide(Money money) {
+		return new Money(amount / money.amount);
+	}
 }
